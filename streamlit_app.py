9290e87ac74459cf7e82400e6f33b0de9b14f739
devr84 <115184161+devr84@users.noreply.github.com>
2023-10-25 06:13:45 +0530

    Update streamlit_app.py
    dsDdSADdsASDa

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ import requests
 import pandas
 from urllib.error import URLError
 
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 streamlit.header("Breakfast Favorites")
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text(' 🥑🍞 Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 
 streamlit.header("🍌🥭 Build your own fruit smoothie 🥑🍞")
 
